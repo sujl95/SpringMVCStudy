78307905414fe2ea05ca888b9b44d1c45986e561
sujl95 <sujl95@naver.com>
2020-11-11 14:49:33 +0900

    Spring MVC 동작원리

---
+++ servletdemo/src/main/java/kr/thewing/WebApplication.java
@@ -0,0 +1,23 @@
+package kr.thewing;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebApplication implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class); //ApplicationContext가 만들어짐
+        context.refresh();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context); //DispatcherServlet 이 만들어진다
+        ServletRegistration.Dynamic app = servletContext.addServlet("app", dispatcherServlet);
+        app.addMapping("/app/*");  //이렇게 코드구현하면 web.xml과 같다
+    }
+}
