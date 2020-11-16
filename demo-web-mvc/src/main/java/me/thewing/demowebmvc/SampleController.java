8298b9ca4b4e1cdbe18eada6b345851e2e3e4799
sujl95 <sujl95@naver.com>
2020-11-16 21:02:50 +0900

    핸들러 메소드 9부: 멀티 폼 서브밋

--- demo-web-mvc/src/main/java/me/thewing/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/me/thewing/demowebmvc/SampleController.java
@@ -17,33 +17,39 @@ import java.util.List;
 @SessionAttributes("event")
 public class SampleController {
 
-    @GetMapping("/events/form")
-    public String eventsForm(Model model) {
+    @GetMapping("/events/form/name")
+    public String eventsFormName(Model model) {
         Event newEvent = new Event();
-        newEvent.setLimit(50);
-        model.addAttribute("event", newEvent); //모델 addAttribute와 SessionAttributes 키 값이 같다면 동일하게 넣어준다
-        return "events/form";
+        model.addAttribute("event", new Event()); //모델 addAttribute와 SessionAttributes 키 값이 같다면 동일하게 넣어준다
+        return "events/form-name";
     }
 
-//    @PostMapping("/events")
-//    @ResponseBody
-//    public Event getEvent(@RequestBody Event event) {
-////        Event event = new Event();
-////        event.setName(name);
-////        event.setLimit(limit);
-//        return event;
-//    }
-    @PostMapping("/events")
-//    @ResponseBody
-    public String createEvent(@Validated @ModelAttribute Event event,
-                           BindingResult bindingResult,
-                           SessionStatus sessionStatus) {
+    @PostMapping("/events/form/name")
+    public String eventsFormNameSubmit(@Validated @ModelAttribute Event event,
+                                       BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "/events/form";
+            return "/events/form-name";
+        }
+        return "redirect:/events/form/limit";
+    }
+
+    @GetMapping("/events/form/limit")
+    public String eventsFormLimit(@ModelAttribute Event event, Model model) {
+        model.addAttribute("event", event);
+        return "/events/form-limit";
+    }
+
+    @PostMapping("/events/form/limit")
+    public String eventsFormLimitSubmit(@Validated @ModelAttribute Event event,
+                                        BindingResult bindingResult,
+                                        SessionStatus sessionStatus) {
+        if (bindingResult.hasErrors()) {
+            return "/events/form-limit";
         }
         sessionStatus.setComplete();
         return "redirect:/events/list";
     }
+
 
     @GetMapping("/events/list")
     public String getEvents(Model model) {
@@ -57,73 +63,5 @@ public class SampleController {
         model.addAttribute(eventList);
         return "/events/list";
     }
-//    @GetMapping("/hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "hello";
-//    }
-//
-//    @PostMapping("/hello")
-//    @ResponseBody
-//    public String helloPost() {
-//        return "hello";
-//    }
-//
-//    @PostMapping(
-//            value = "/events",
-//            consumes = MediaType.APPLICATION_JSON_VALUE
-//    )
-//    @ResponseBody
-//    public String createEvent() {
-//        return "events";
-//    }
-//
-//
-//    @GetMapping("/events/{id}")
-//    @ResponseBody
-//    public String getAnEvents(@PathVariable Long id) {
-//        return "events";
-//    }
-//
-//    @DeleteMapping("/events/{id}")
-//    @ResponseBody
-//    public String deleteAnEvents(@PathVariable Long id) {
-//        return "events";
-//    }
-//
-//    @PutMapping(
-//            value = "/events",
-//            consumes = MediaType.APPLICATION_JSON_VALUE
-//    )
-//    @ResponseBody
-//    public String updateEvent() {
-//        return "events";
-//    }
-//
-//    @RequestMapping(value = "/hello",method = {RequestMethod.GET,RequestMethod.PUT}) //
-//    @GetMapping({"/hello" , "/hi"}) //이렇게도 사용가능
-//    @GetMapping("/hello")
-//    @RequestMapping("/{name:[a-z]+}")
-//    @ResponseBody //문자열을 응답
-//    public String hello(@PathVariable String name) {
-//        return "hello "+name; //해당하는 뷰를 찾는다
-//    }
-//    @RequestMapping({"/sungjun", "/sungjun.*"})
-//    @ResponseBody
-//    public String helloSungjun() {
-//        return "hello sungjun";
-//    }
-//
-//    @RequestMapping("/**")
-//    @ResponseBody
-//    public String hello() {
-//        return "hello";
-//    }
-//
-//    @RequestMapping("/sungjun")
-//    @ResponseBody
-//    public String helloSungjun() {
-//        return "hello sungjun";
-//    }
 }
 
