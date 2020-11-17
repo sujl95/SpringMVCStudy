565b09bb18466413f7e36ca4b62d348084a422e0
sujl95 <sujl95@naver.com>
2020-11-17 15:15:07 +0900

    핸들러 메소드 14부 : ResponseEntity

--- demo-web-mvc/src/main/java/me/thewing/demowebmvc/FIleController.java
+++ demo-web-mvc/src/main/java/me/thewing/demowebmvc/FIleController.java
@@ -1,15 +1,28 @@
 package me.thewing.demowebmvc;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FIleController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model) {
@@ -23,5 +36,19 @@ public class FIleController {
         String message = file.getOriginalFilename() + " is uploaded";
         attributes.addFlashAttribute("message", message);
         return "redirect:/file";
+    }
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+resource.getFilename()+"\"") //다운 받을때 팝업이 열리면서 이름을 지정해준다
+                    .header(HttpHeaders.CONTENT_TYPE,"text/txt") //파일 타입
+                    .header(HttpHeaders.CONTENT_LENGTH, file.length()+"") //파일 길이
+                    .body(resource);
     }
 }
