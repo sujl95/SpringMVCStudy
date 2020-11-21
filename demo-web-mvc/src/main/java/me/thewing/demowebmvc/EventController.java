b6e0498bc5e0f19027d6f42a7f7df5f4a7b773e7
sujl95 <sujl95@naver.com>
2020-11-22 00:01:17 +0900

    add

--- demo-web-mvc/src/main/java/me/thewing/demowebmvc/EventController.java
+++ demo-web-mvc/src/main/java/me/thewing/demowebmvc/EventController.java
@@ -1,6 +1,7 @@
 package me.thewing.demowebmvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +18,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-
+@Component
 @Controller
 @SessionAttributes("event")
 public class EventController {
