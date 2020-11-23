2b287035afbef69e012e3b07742c3e7825136c21
sujl95 <sujl95@naver.com>
2020-11-24 00:02:39 +0900

    memo create

--- demo-web-mvc/src/test/java/me/thewing/demowebmvc/DemoWebMvcApplicationTests.java
+++ demo-web-mvc/src/test/java/me/thewing/demowebmvc/DemoWebMvcApplicationTests.java
@@ -1,7 +1,10 @@
 package me.thewing.demowebmvc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class DemoWebMvcApplicationTests {
